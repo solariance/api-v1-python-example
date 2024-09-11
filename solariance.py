0ad74d30e18185ad0713336810910fb9332e7f67
Oli <oliver.moenius@fau.de>
2024-09-11 09:36:36 +0200

    Removed MIT License

--- solariance.py
+++ solariance.py
@@ -2,14 +2,15 @@ import requests
 import os
 from requests.exceptions import HTTPError
 
-SOLARIANCE_USER = os.environ.get("SOLARIANCE_USER")
-SOLARIANCE_PWD = os.environ.get("SOLARIANCE_PWD")
-SYSTEM_ID = os.environ.get("SYSTEM_ID")
+SOLARIANCE_USER    = os.environ.get("SOLARIANCE_USER")
+SOLARIANCE_PWD     = os.environ.get("SOLARIANCE_PWD")
+SYSTEM_ID          = os.environ.get("SYSTEM_ID")
 
-SOLARIANCE_API_V1 = "https://api.solariance.de/v1"
+SOLARIANCE_API_V1  = "https://api.solariance.de/v1"
 USER_AUTH_WITH_PWD = "user/auth/pw"
-FORECAST_POWER = "forecast/power" 
-PV_SYSTEM_VIEW = "system/view"
+FORECAST_POWER     = "forecast/power"
+PV_SYSTEM_VIEW     = "system/view"
+
 
 def solariance_api_call(method: str, endpoint: str, headers: dict, body: dict, params: dict) -> dict:
     """
@@ -48,6 +49,7 @@ def solariance_api_call(method: str, endpoint: str, headers: dict, body: dict, p
         print(f'An unexpected error occurred: {e}')
     return {}
 
+
 def get_user_jwt_token(user: str, pwd: str) -> str:
     """
     Retrieves a JWT token for the specified user.
@@ -63,16 +65,17 @@ def get_user_jwt_token(user: str, pwd: str) -> str:
     response = solariance_api_call(
         method="POST",
         endpoint=USER_AUTH_WITH_PWD,
-        headers={}, 
+        headers={},
         body={
             "identity": user,
             "password": pwd
         },
         params={})
-    
+
     return response.get("data").get("token")
 
-def get_forecast_power(token: str, system_id:str) -> dict:
+
+def get_forecast_power(token: str, system_id: str) -> dict:
     """
     Retrieves the forecast power for a given system ID.
 
@@ -89,15 +92,16 @@ def get_forecast_power(token: str, system_id:str) -> dict:
         endpoint=FORECAST_POWER,
         headers={
             "Authorization": token
-        }, 
+        },
         body={},
         params={
             "system_id": system_id
         })
-    
+
     return response
 
-def get_system_view(token: str, system_id:str) -> dict:
+
+def get_system_view(token: str, system_id: str) -> dict:
     """
     Retrieves information about a PV system.
 
@@ -114,13 +118,14 @@ def get_system_view(token: str, system_id:str) -> dict:
         endpoint=PV_SYSTEM_VIEW,
         headers={
             "Authorization": token
-        }, 
+        },
         body={},
         params={
             "system_id": system_id
         })
-    
+
     return response
+
 
 def main():
     """
@@ -132,7 +137,9 @@ def main():
     print(f"View PV System {SYSTEM_ID}: {pv_system}\n")
     forecast_power = get_forecast_power(token, SYSTEM_ID)
     # Print the forecasted power for the first time period on the first day
-    print(f"PV Power Forecast for {SYSTEM_ID}: {forecast_power.get('data').get('forecast')[0][0]}")
+    print(
+        f"PV Power Forecast for {SYSTEM_ID}: {forecast_power.get('data').get('forecast')[0][0]}")
+
 
 if __name__ == "__main__":
     main()
